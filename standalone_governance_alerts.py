0e4b34c27c07fe990445d3624442c4369bc8bc23
Thomas Lafaille <thomas.lafaille@gmail.com>
2022-03-22 07:52:15 +0100

    Corrected an error, added a regex for parsing the link to the proposal (Terra)

--- standalone_governance_alerts.py
+++ standalone_governance_alerts.py
@@ -7,6 +7,7 @@ from terra_sdk.client.lcd import LCDClient
 from terra_sdk.core.gov import ProposalStatus
 from argparse import ArgumentParser
 from requests import get
+from re import findall
 
 class GetNewTerraProposals:
     """A simple class to regularly check for new proposals (in the voting period) for Terra.
@@ -23,13 +24,13 @@ class GetNewTerraProposals:
                     terra = LCDClient(chain_id="columbus-5", url="https://lcd.terra.dev")
 
                     try:
-                        proposals = terra.gov.proposals({"proposal_status": ProposalStatus(2)})
+                        proposals = terra.gov.proposals({"proposal_status": ProposalStatus(2)}) #status(2) = voting period
 
                         for i in proposals[0]:
                             if i.submit_time > self.now:  # the proposal entered the voting period after the previous check, so we should alert.
-                                link = i.content.description.split('\n')[0] #the link to the proposal
+                                link = findall('(https:\/\/agora\S+)', i.content.description)[0]#.split('\n')[0] #the link to the proposal
                                 number = i.proposal_id
-                                message = f"{i.upper()}\nProposal {number} is in voting stage!\n{link}"
+                                message = f"{i.content.title.upper()}\nProposal {number} is in voting stage!\n{link}"
                                 DiscordWebhook(url=self.discord_url, content=message).execute()
 
                             #now let's update the timestamp so that only later proposals are caught.
